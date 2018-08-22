1775731fd18fddaaaf250507e3edc273629fd68e
Anish Shah <anish.shah01@sap.com>
2018-08-22 17:52:18 +0000

    Student Schedule changes

--- webapp/controller/StudentSchedule.controller.js
+++ webapp/controller/StudentSchedule.controller.js
@@ -1,9 +1,22 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define( ["sap/ui/core/mvc/Controller","sap/ui/core/routing/History", "sap/ui/Device"], function (Controller, History, Device) {
 	"use strict";
 
 	return Controller.extend("STARSchedule.STARSchedule.controller.StudentSchedule", {
+		onInit : function () {
+			this.getOwnerComponent().getRouter().getRoute("StudentSchedule").attachPatternMatched(this._onRouteMatched, this);
+		},
+		_onRouteMatched: function(oEvent) {
+			//this._coopID = oEvent.getParameter("arguments").coopID;
+			this.user = '0';
+			this.getView().bindElement("/StudentSchedule/" + this.user);
+		},
+		onSelectionChange: function(oEvent) {
+	
+		},
+		onNavBack : function() {
+			var sPreviousHash = History.getInstance().getPreviousHash();
+		}
 
 	});
-});
+
+}, /* bExport= */ true);
