e56f66bc7debe36a511245c348ef1d23e7a846b7
Anish Shah <anish.shah01@sap.com>
2018-08-22 19:26:11 +0000

    Student Schedule

--- webapp/controller/ManagerInternTable.controller.js
+++ webapp/controller/ManagerInternTable.controller.js
@@ -11,7 +11,7 @@ sap.ui.define([
 			},
 			onItemPressed:function(oEvent){
 				var sKey=oEvent.getParameter("listItem").getId().substr(-1);
-				this.getRouter().navTo("StudentSchedule", {
+				this.getOwnerComponent().getRouter().navTo("StudentSchedule", {
 					Muser : "0",
 					Iuser : sKey
 				});
