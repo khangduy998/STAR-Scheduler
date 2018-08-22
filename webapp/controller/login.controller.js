1c9653c613b2003a5f10eefa6c98f0342a8e3066
I852870 <purav.barot@sap.com>
2018-08-22 18:27:14 +0000

    new commit
    Change-Id: I66a893a539fee4b9b78e2124088ff4ba395a7b52

--- webapp/controller/login.controller.js
+++ webapp/controller/login.controller.js
@@ -1,14 +1,57 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+		'jquery.sap.global',
+		'sap/ui/core/mvc/Controller',
+		'sap/ui/model/json/JSONModel',
+			'sap/m/Popover',
+			'sap/m/Button'
+	], function(jQuery, Controller, JSONModel, Popover, Button) {
 	"use strict";
 
-	return Controller.extend("STARSchedule.login.controller.login", {
+	var PageController = Controller.extend("STARSchedule.STARSchedule.controller.login", {
 		
-		onInit: function(){
-			this.getView().setModel(this.getOwnerComponent().getModel(),"mainModel");
-		}
 
+		handleEditPress : function (evt) {
+			var oTileContainer = this.byId("container");
+			var newValue = !oTileContainer.getEditable();
+			oTileContainer.setEditable(newValue);
+			evt.getSource().setText(newValue ? "Done" : "Edit");
+		},
+
+		handleBusyPress : function (evt) {
+			var oTileContainer = this.byId("container");
+			var newValue = !oTileContainer.getBusy();
+			oTileContainer.setBusy(newValue);
+			evt.getSource().setText(newValue ? "Done" : "Busy state");
+		},
+
+		handleTileDelete : function (evt) {
+			var tile = evt.getParameter("tile");
+			evt.getSource().removeTile(tile);
+		},
+		onUserNamePress: function (oEvent) {
+			var oPopover = new Popover({
+				showHeader: false,
+				placement: sap.m.PlacementType.Bottom,
+				content:[
+					new Button({
+						text: 'Feedback',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Help',
+						type: sap.m.ButtonType.Transparent
+					}),
+					new Button({
+						text: 'Logout',
+						type: sap.m.ButtonType.Transparent
+					})
+				]
+			}).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
+
+			oPopover.openBy(oEvent.getSource());
+		}
 	});
+
+	return PageController;
 
 });
