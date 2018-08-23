203776064f23e06704174125e25b91e99fd05cc7
I852870 <purav.barot@sap.com>
2018-08-23 20:46:52 +0000

    latest changes with auth working
    Change-Id: I4bc63dfe25676f654d57828460500efee44acec2

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,20 +7,53 @@ sap.ui.define([
 			
 			
 			onLoggedIn: function(){
-			 if(this.getView().byId("username").getValue() == "" || this.getView().byId("username").getValue() != "june") {
-
-                           alert("Please enter your username");
-				 }
-			else if(this.getView().byId("password").getValue() == "" || this.getView().byId("password").getValue() != "june"){
-						
-						alert("Please enter your password");
+				//getting username and password from Managers.json
+				var ManagerModel = this.getOwnerComponent().getModel("Managers");
+				var ManagerUsername = ManagerModel.getProperty("/0/Username");
+				var ManagerPassword = ManagerModel.getProperty("/0/Password");
 				
+				
+
+				var Intern = ManagerModel.getProperty("/0/Intern");
+			/*	var InternUsername = ManagerModel.getProperty("/0/Intern/0/Username");
+				var InternPassword = ManagerModel.getProperty("/0/Intern/0/Password");*/
+			
+			
+				
+			for(var i = 0; i < Intern.length; i++){
+				
+			var	InternUsername1 = Intern[i].Username;
+			var	InternPassword1 = Intern[i].Password;
+			
+			if(this.getView().byId("username").getValue() == ManagerUsername && this.getView().byId("password").getValue() == ManagerPassword) {
+                          this.ManagerviewRouter();
+                          break;
+				 }
+			
+			else if(this.getView().byId("username").getValue() == InternUsername1 && this.getView().byId("password").getValue() == InternPassword1){
+						this.InternviewRouter();
+						break;
+						
+			}
+			else if(i==Intern.length-1){
+				alert("Enter correct Username and Password");
 			}
 			else{
 				
+				
+				continue;
+				
+			}
+
+			}
+				
+			},
+			ManagerviewRouter: function(){
 				this.getOwnerComponent().getRouter().navTo("LogInView");
-				console.log(this.getView().getModel());
+			},
+			InternviewRouter: function(){
+				this.getOwnerComponent().getRouter().navTo("internView");
 			}
-			}
+			
 	});
 });
